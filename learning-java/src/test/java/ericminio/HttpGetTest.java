893e5e13af1071a252afc9fcc2174670f2e72507
ericminio <eric.mignot@gmail.com>
2017-03-26 17:37:56 -0400

    first doc for http get and post

---
+++ learning-java/src/test/java/ericminio/HttpGetTest.java
@@ -0,0 +1,80 @@
+package ericminio;
+
+import com.sun.net.httpserver.HttpServer;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.InetSocketAddress;
+import java.net.URL;
+
+import static org.hamcrest.core.IsEqual.equalTo;
+import static org.junit.Assert.assertThat;
+
+public class HttpGetTest {
+
+    private HttpServer server;
+
+    @Before
+    public void startServer() throws Exception {
+        server = HttpServer.create( new InetSocketAddress( 8000 ), 0 );
+        server.createContext( "/", exchange -> {
+            exchange.sendResponseHeaders( 200, 0 );
+            exchange.close();
+        } );
+        server.createContext( "/json", exchange -> {
+            exchange.getResponseHeaders().add( "content-type", "application/json" );
+            exchange.sendResponseHeaders( 200, 0 );
+            exchange.close();
+        } );
+        server.createContext( "/need", exchange -> {
+            String body = "Love";
+            exchange.sendResponseHeaders( 200, body.length() );
+            exchange.getResponseBody().write( body.getBytes() );
+            exchange.close();
+        } );
+        server.start();
+    }
+
+    @After
+    public void stopServer() {
+        server.stop( 0 );
+    }
+
+    @Test
+    public void canAnswerOKForAnyGetRequest() throws Exception {
+        URL url = new URL( "http://localhost:8000" );
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+
+        assertThat( connection.getResponseCode(), equalTo( 200 ) );
+    }
+
+    @Test
+    public void answersWithoutContentTypeByDefault() throws Exception {
+        URL url = new URL( "http://localhost:8000" );
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+
+        assertThat( connection.getHeaderField( "content-type" ), equalTo( null ) );
+    }
+
+    @Test
+    public void canAnswerWithJsonContentType() throws Exception {
+        URL url = new URL( "http://localhost:8000/json" );
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+
+        assertThat( connection.getHeaderField( "content-type" ), equalTo( "application/json" ) );
+    }
+
+    @Test
+    public void canSendContent() throws Exception {
+        URL url = new URL( "http://localhost:8000/need" );
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        InputStream inputStream = connection.getInputStream();
+        byte[] response = new byte[ inputStream.available() ];
+        inputStream.read( response );
+
+        assertThat( new String( response ), equalTo( "Love" ) );
+    }
+}
---
+++ learning-java/src/test/java/ericminio/HttpPostTest.java
@@ -0,0 +1,63 @@
+package ericminio;
+
+import com.sun.net.httpserver.HttpServer;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.InetSocketAddress;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.util.stream.Collectors;
+
+import static org.hamcrest.core.IsEqual.equalTo;
+import static org.junit.Assert.assertThat;
+
+public class HttpPostTest {
+
+    private HttpServer server;
+
+    @Before
+    public void startServer() throws Exception {
+        server = HttpServer.create( new InetSocketAddress( 8000 ), 0 );
+        server.createContext( "/create", exchange -> {
+            String method = exchange.getRequestMethod();
+            BufferedReader data = new BufferedReader(new InputStreamReader(exchange.getRequestBody()));
+            String body = "method=" + method + " data=" + data.lines().collect(Collectors.joining());
+            exchange.sendResponseHeaders( 200, body.length() );
+            exchange.getResponseBody().write( body.getBytes() );
+            exchange.close();
+        } );
+        server.start();
+    }
+
+    @After
+    public void stopServer() {
+        server.stop( 0 );
+    }
+
+    @Test
+    public void canSendDataViaPost() throws Exception {
+        URL url = new URL( "http://localhost:8000/create" );
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        connection.setDoOutput(true);
+        connection.setRequestMethod("POST");
+        byte[] postData = "key".getBytes( StandardCharsets.UTF_8 );
+        connection.setRequestProperty( "Content-Length", Integer.toString(postData.length));
+        DataOutputStream writer = new DataOutputStream( connection.getOutputStream());
+        writer.write(postData);
+
+        assertThat( connection.getResponseCode(), equalTo( 200 ) );
+
+        InputStream inputStream = connection.getInputStream();
+        byte[] response = new byte[ inputStream.available() ];
+        inputStream.read( response );
+
+        assertThat( new String( response ), equalTo( "method=POST data=key" ) );
+    }
+}

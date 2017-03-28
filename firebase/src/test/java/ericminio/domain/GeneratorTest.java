bc2b3d0912e588ed5c18c6d9c1ae0cd4ea42a8f2
ericminio <eric.mignot@gmail.com>
2017-03-27 20:15:23 -0400

    news generator fetches users and games from db

--- firebase/src/test/java/ericminio/domain/GeneratorTest.java
+++ firebase/src/test/java/ericminio/domain/GeneratorTest.java
@@ -2,6 +2,8 @@ package ericminio.domain;
 
 import ericminio.firebase.Entry;
 import ericminio.firebase.Firebase;
+import ericminio.firebase.FirebaseRepository;
+import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Test;
 
@@ -10,20 +12,26 @@ import java.util.Arrays;
 public class GeneratorTest {
 
     private Firebase database = new Firebase("https://sandbox-5f095.firebaseio.com/");
+    RandomEvent randomEvent;
+
+    @Before
+    public void getData() throws Exception {
+        FirebaseRepository storage = new FirebaseRepository(database);
+        randomEvent = new RandomEvent();
+        randomEvent.setUsers(storage.allUsers());
+        randomEvent.setGames(storage.allGames());
+        randomEvent.setEvents(Arrays.asList(new LoggedIn(), new LevelUp(), new GameWin()));
+    }
 
     @Ignore
     @Test
     public void go() throws Exception {
-        RandomEvent randomEvent = new RandomEvent();
-        randomEvent.setUsers(Arrays.asList("Tom67", "MeaThoN", "lyllikka", "dragon123"));
-        randomEvent.setGames(Arrays.asList("Book of dead", "Thunderstruck II", "Well of wonder"));
-        randomEvent.setEvents(Arrays.asList(new LoggedIn(), new LevelUp(), new GameWin()));
 
         while(true) {
             long timestamp = System.currentTimeMillis();
             String data = "{\"message\":\""+ randomEvent.please().description +"\",\"timestamp\":"+timestamp+"}";
             database.save(new Entry("news/" + timestamp, data));
-            Thread.sleep(3000);
+            Thread.sleep(5000);
         }
     }
 }

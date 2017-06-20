ed2108f5bcc52050d249a3e24a29aa3a07dbbc02
Perso <perso@Erics-MBP.omega.local>
2017-06-20 08:36:33 -0700

    first documentation of @Configuration used in static inner class

---
+++ spring-boot-test/src/main/java/ericminio/domain/Something.java
@@ -0,0 +1,4 @@
+package ericminio.domain;
+
+public class Something {
+}
---
+++ spring-boot-test/src/test/java/ericminio/AutowiredTest.java
@@ -0,0 +1,33 @@
+package ericminio;
+
+import ericminio.domain.Something;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.assertj.core.api.Java6Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
+public class AutowiredTest {
+
+    @Autowired
+    Something something;
+
+    @Test
+    public void configurationCanInjectNativeBean() {
+        assertThat(something).isNotNull();
+    }
+
+    @Configuration
+    static class AutowiredBeanFactory {
+
+        @Bean
+        public Something knowsHowToBuildSomething() {
+            return new Something();
+        }
+    }
+}

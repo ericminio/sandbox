a6db5d23480c58b405f214bb9ae166469e4f7cb2
Eric Mignot <eric.mignot@gmail.com>
2019-07-04 20:38:47 -0700

    Introduce endpoint variable to remove Duplication

--- spring-boot-test/src/test/java/ericminio/http/HttpGetTest.java
+++ spring-boot-test/src/test/java/ericminio/http/HttpGetTest.java
@@ -18,11 +18,13 @@ import static org.springframework.boot.test.context.SpringBootTest.WebEnvironmen
 @RunWith(SpringRunner.class)
 @SpringBootApplication
 @SpringBootTest(webEnvironment= DEFINED_PORT)
-public class GreetingTest {
+public class HttpGetTest {
+
+    private String endpoint = "http://localhost:8080/greeting";
 
     @Test
     public void isAvailable() throws Exception {
-        URL url = new URL( "http://localhost:8080/greeting" );
+        URL url = new URL(endpoint);
         HttpURLConnection connection = (HttpURLConnection) url.openConnection();
 
         assertThat( connection.getResponseCode(), equalTo( 200 ) );
@@ -30,21 +32,21 @@ public class GreetingTest {
 
     @Test
     public void answersWithJson() throws Exception {
-        HttpResponse response = get("http://localhost:8080/greeting" );
+        HttpResponse response = get(endpoint);
 
         assertThat( response.getContentType(), equalTo( "application/json;charset=UTF-8" ) );
     }
 
     @Test
     public void sendsGreetings() throws Exception {
-        HttpResponse response = get( "http://localhost:8080/greeting?name=Joe" );
+        HttpResponse response = get( endpoint + "?name=Joe" );
 
         assertThat( response.getBody(), equalTo( "{\"content\":\"Hello, Joe!\"}" ) );
     }
 
     @Test
     public void defaultGreetings() throws Exception {
-        HttpResponse response = get( "http://localhost:8080/greeting" );
+        HttpResponse response = get(endpoint);
 
         assertThat( response.getBody(), equalTo( "{\"content\":\"Hello, World!\"}" ) );
     }

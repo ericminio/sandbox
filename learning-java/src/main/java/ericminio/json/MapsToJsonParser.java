4339212d4a155711aeac159ab304b23979e63a2f
Eric Mignot <eric.mignot@gmail.com>
2020-01-30 16:22:47 -0800

    ExtractMethod quote to remove Duplication

--- learning-java/src/main/java/ericminio/json/MapsToJsonParser.java
+++ learning-java/src/main/java/ericminio/json/MapsToJsonParser.java
@@ -1,7 +1,6 @@
 package ericminio.json;
 
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -11,23 +10,25 @@ public class MapsToJsonParser {
     public static String stringify(Object o) {
         if (o == Boolean.TRUE) { return "true"; }
         if (o == Boolean.FALSE) { return "false"; }
-        if (o instanceof String) { return "\"" + o + "\""; }
+        if (o instanceof String) { return quote((String) o); }
         if (o instanceof List) { return stringifyCollection((List) o); }
+
         if (! (o instanceof Map)) { return o.toString(); }
 
-        Map<String, Object> tree = (Map) o;
         List<String> fields = new ArrayList<>();
-        Iterator<String> keys = tree.keySet().iterator();
-        while(keys.hasNext()) {
-            String key = keys.next();
-            Object value = tree.get(key);
-            fields.add("\"" + key + "\":" + stringify(value));
-        }
+        Map<String, Object> map = (Map) o;
+        map.forEach((key, value)-> {
+            fields.add(quote(key) + ":" + stringify(value));
+        });
 
         return "{" + fields.stream().collect(Collectors.joining(",")) + "}";
     }
 
     private static String stringifyCollection(List list) {
         return "[" + list.stream().map(e -> stringify(e)).collect(Collectors.joining(",")) + "]";
+    }
+
+    private static String quote(String key) {
+        return "\"" + key + "\"";
     }
 }

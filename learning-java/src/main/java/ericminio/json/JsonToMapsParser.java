4a40a099a22ad7ae7e9ad13a69555ffe6862d87a
Eric Mignot <eric.mignot@gmail.com>
2020-02-04 15:42:35 -0800

    support (.*) to capture groups

--- learning-java/src/main/java/ericminio/json/JsonToMapsParser.java
+++ learning-java/src/main/java/ericminio/json/JsonToMapsParser.java
@@ -95,6 +95,8 @@ public class JsonToMapsParser {
                         .replace("]", "\\]")
                         .replace("(", "\\(")
                         .replace(")", "\\)")
+                        .replace(".", "\\.")
+                        .replace("*", "\\*")
                 , "");
     }
 

c6d683664326f3733c1726f00889d2e94600f475
Eric Mignot <eric.mignot@gmail.com>
2020-02-05 12:54:38 -0800

    first documentation of base64 encoding/decoding

---
+++ learning-java/src/test/java/ericminio/Base64Test.java
@@ -0,0 +1,31 @@
+package ericminio;
+
+import org.junit.Test;
+
+import java.util.Base64;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.startsWith;
+import static org.junit.Assert.assertThat;
+
+public class Base64Test {
+
+    @Test
+    public void canEncode() {
+        assertThat(Base64.getEncoder().encodeToString("hello".getBytes()), equalTo("aGVsbG8="));
+    }
+
+    @Test
+    public void canDecode() {
+        byte[] decoded = Base64.getDecoder().decode("aGVsbG8=");
+
+        assertThat(new String(decoded), equalTo("hello"));
+    }
+
+    @Test
+    public void canBeMisleading() {
+        byte[] decoded = Base64.getDecoder().decode("aGVsbG8=");
+
+        assertThat(String.valueOf(decoded), startsWith("[B@"));
+    }
+}

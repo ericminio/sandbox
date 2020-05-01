ce1f570e7002f0ececf9f465fc22cdeead301ac6
ericminio <eric.mignot@gmail.com>
2020-05-01 10:29:18 -0700

    more about date formatting

--- learning-java/src/test/java/ericminio/DateTimeFormatterTest.java
+++ learning-java/src/test/java/ericminio/DateTimeFormatterTest.java
@@ -4,6 +4,8 @@ import org.junit.Test;
 
 import java.time.LocalDateTime;
 import java.time.Month;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
 
 import static org.hamcrest.CoreMatchers.equalTo;
@@ -17,5 +19,12 @@ public class DateTimeFormatterTest {
         LocalDateTime dateTime = LocalDateTime.of(2015, Month.DECEMBER, 15, 0, 0);
 
         assertThat(dateTime.format(formatter), equalTo("2015-12-15"));
+    }
+    @Test
+    public void zuluTime() {
+        ZonedDateTime local = LocalDateTime.of(2015, Month.JANUARY, 15, 19, 15).atZone(ZoneId.of("UTC-6"));
+        ZonedDateTime utc = ZonedDateTime.ofInstant(local.toInstant(), ZoneId.of("UTC"));
+
+        assertThat(DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSSXX").format(utc), equalTo("2015-01-16T01:15:00.000Z"));
     }
 }

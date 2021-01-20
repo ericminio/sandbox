fc81008b91c8900d2710166c34bc1da271dacde2
ericminio <eric.mignot@gmail.com>
2021-01-20 11:37:58 -0800

    unused code removed

--- spring-boot-test/src/test/java/ericminio/demo/security/HasAuthoritiesTest.java
+++ spring-boot-test/src/test/java/ericminio/demo/security/HasAuthoritiesTest.java
@@ -3,14 +3,12 @@ package ericminio.demo.security;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.web.client.RestTemplate;
 
 import static org.hamcrest.core.IsEqual.equalTo;
 import static org.junit.Assert.assertThat;
@@ -22,9 +20,6 @@ public class HasAuthoritiesTest {
 
     @LocalServerPort
     int port;
-
-    @Autowired
-    RestTemplate restTemplate;
 
     private String needsAuthority;
 
